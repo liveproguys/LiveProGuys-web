f2e0cb549bfdd3e06521ef5c57a1070bff5e40e2
LiveProGuys Group <jahongirmutual@gmail.com>
2025-04-24 16:17:10 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return "Salom"
+    return render_template("index.html")
 app.run(debug=True,host="0.0.0.0")
