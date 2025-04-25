2d65868af4c99fb1299d75f1f1661ad7c355bb81
LiveProGuys Group <jahongirmutual@gmail.com>
2025-04-25 09:49:08 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,19 @@
-from flask import Flask,render_template
-app=Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 @app.route("/about")
-def abaut():
+def about():
     return render_template("Portfilio.html")
-app.run(debug=True,host="0.0.0.0")
+
+# 404 xato sahifasi uchun handler
 @app.errorhandler(404)
-def handle(error):
+def handle_404(error):
     return render_template('error.html'), 404
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
